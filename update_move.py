31fd0fd80c34ad7c305e28ba911203385edf3e5f
Haydost <alimohammadi1991@outlook.com>
2022-08-02 01:08:32 +0430

    flake8 has been checked

--- update_move.py
+++ update_move.py
@@ -17,32 +17,40 @@ def update_grid(new_player_index: int, n: int) -> List:
         print(row_i_new)
     locations = [(x, y) for x in range(1, n+1) for y in range(1, n+1)]
     cordinations = dict(zip(available_places_indexes, locations))
-    print(
-        f"\nYou are currently located in room: {cordinations[new_player_index]}")
+    print("\nYou are currently located in room: {}".format(
+        cordinations[new_player_index]))
     limitations = []
     if cordinations[new_player_index][0] == 1:
         limitations.append('UP')
-        if cordinations[new_player_index][0] == 1 and cordinations[new_player_index][1] == 1:
+        if cordinations[new_player_index][0] == 1 and\
+                cordinations[new_player_index][1] == 1:
             limitations.append('LEFT')
-        elif cordinations[new_player_index][0] == 1 and cordinations[new_player_index][1] == n:
+        elif cordinations[new_player_index][0] == 1 and\
+                cordinations[new_player_index][1] == n:
             limitations.append('RIGHT')
     elif cordinations[new_player_index][0] == n:
         limitations.append('DOWN')
-        if cordinations[new_player_index][0] == n and cordinations[new_player_index][1] == 1:
+        if cordinations[new_player_index][0] == n and\
+                cordinations[new_player_index][1] == 1:
             limitations.append('LEFT')
-        elif cordinations[new_player_index][0] == n and cordinations[new_player_index][1] == n:
+        elif cordinations[new_player_index][0] == n and\
+                cordinations[new_player_index][1] == n:
             limitations.append('RIGHT')
     elif cordinations[new_player_index][1] == 1:
         limitations.append('LEFT')
-        if cordinations[new_player_index][1] == 1 and cordinations[new_player_index][0] == 1:
+        if cordinations[new_player_index][1] == 1 and\
+                cordinations[new_player_index][0] == 1:
             limitations.append('UP')
-        elif cordinations[new_player_index][1] == 1 and cordinations[new_player_index][0] == n:
+        elif cordinations[new_player_index][1] == 1 and\
+                cordinations[new_player_index][0] == n:
             limitations.append('DOWN')
     elif cordinations[new_player_index][1] == n:
         limitations.append('RIGHT')
-        if cordinations[new_player_index][1] == n and cordinations[new_player_index][0] == 1:
+        if cordinations[new_player_index][1] == n and\
+                cordinations[new_player_index][0] == 1:
             limitations.append('UP')
-        elif cordinations[new_player_index][1] == n and cordinations[new_player_index][0] == n:
+        elif cordinations[new_player_index][1] == n and\
+                cordinations[new_player_index][0] == n:
             limitations.append('DOWN')
     else:
         print("You can move in all four directions")
@@ -50,8 +58,8 @@ def update_grid(new_player_index: int, n: int) -> List:
         if len(limitations) == 1:
             print(f"You can't move '{limitations[0]}'")
         else:
-            print(
-                f"You can't move either '{limitations[0]}' and '{limitations[1]}'")
+            print("You can't move either '{}' and '{}'".format(
+                limitations[0], limitations[1]))
     valuable_info = [new_player_index, limitations,
                      cordinations, rows_list, available_places_indexes]
-    return(valuable_info)
+    return (valuable_info)
